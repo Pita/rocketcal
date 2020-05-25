2a59e15ab44574da10e6bc39b044b9e2b9c7ba23
Peter 'Pita' Martischka <petermartischka@gmail.com>
2020-05-25 07:02:16 +0200

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: "./src/handler.ts",
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
