0849527f26a419c9ecee9bb34edb6a99eb695e9a
Peter 'Pita' Martischka <petermartischka@gmail.com>
2020-05-25 08:21:07 +0200

    Fixed webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,42 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const slsw = require("serverless-webpack");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+const isLocal = slsw.lib.webpack.isLocal;
+
 module.exports = {
-  entry: "./src/handler.ts",
+  mode: isLocal ? "development" : "production",
+  entry: slsw.lib.entries,
+  externals: [nodeExternals()],
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
+  output: {
+    libraryTarget: "commonjs2",
+    path: path.join(__dirname, ".webpack"),
+    filename: "[name].js",
+  },
   target: "node",
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        use: "ts-loader",
+        // Include ts, tsx, js, and jsx files.
+        test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
+        use: [
+          {
+            loader: "cache-loader",
+            options: {
+              cacheDirectory: path.resolve(".webpackCache"),
+            },
+          },
+          "babel-loader",
+        ],
       },
     ],
   },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
+  plugins: [new ForkTsCheckerWebpackPlugin()],
 };
